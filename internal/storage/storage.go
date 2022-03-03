fa9a6ca00ca9ac72179dfbb363a28c851bc9840b
zzell <rsukhodolskyi@gmail.com>
2022-03-03 19:54:32 +0200

    tmp storage

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,4 +1,198 @@
 package storage
 
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"time"
+)
+
+const dialect = "postgres"
+
+type Config struct {
+	DSN string `yaml:"dsn"`
+}
+
 type Interface interface {
+	UpsertUser(*User) (*User, error)
+	SelectLocalities(string) ([]*LocalityRegion, error)
+
+	SelectRequestsByUser(uuid.UUID) ([]*Request, error)
+	InsertRequest(*Request) (*Request, error)
+	ResolveRequest(uuid.UUID) error
+
+	SelectHelpsByUser(uuid.UUID) ([]*Help, error)
+	InsertHelp(*Help) (*Help, error)
+	DeleteHelp(uuid.UUID) error
+}
+
+type Postgres struct {
+	config *Config
+	driver *sqlx.DB
+	ctx    context.Context
+}
+
+func NewPostgres(ctx context.Context, c *Config) (*Postgres, error) {
+	db, err := sqlx.Open(dialect, c.DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Postgres{
+		config: c,
+		driver: db,
+	}, nil
+}
+
+type (
+	User struct {
+		ID        uuid.UUID `db:"id"`
+		TgID      int64     `db:"tg_id"`
+		ChatID    int64     `db:"chat_id"`
+		Name      string    `db:"name"`
+		CreatedAt time.Time `db:"created_at"`
+		UpdatedAt time.Time `db:"updated_at"`
+	}
+
+	Request struct {
+		ID          uuid.UUID `db:"id"`
+		CreatorID   uuid.UUID `db:"creator_id"`
+		CategoryID  uuid.UUID `db:"category_id"`
+		LocalityID  int       `db:"locality_id"`
+		Phone       string    `db:"phone"`
+		Description string    `db:"description"`
+		Resolved    bool      `db:"resolved"`
+		CreatedAt   time.Time `db:"createdAt"`
+	}
+
+	Locality struct {
+		ID           int    `db:"id"`
+		Type         string `db:"type"`
+		NameEN       string `db:"name_en"`
+		NameRU       string `db:"name_ru"`
+		NameUA       string `db:"name_ua"`
+		PublicNameEN string `db:"public_name_en"`
+		PublicNameRU string `db:"public_name_ru"`
+		PublicNameUA string `db:"public_name_ua"`
+		Lng          int64  `db:"lng"`
+		Lat          int64  `db:"lat"`
+		ParentID     int    `db:"parent_id"`
+	}
+
+	LocalityRegion struct {
+		ID         int    `db:"l1.id"`
+		Type       string `db:"l1.type"`
+		Name       string `db:"l1.public_name_ua"`
+		RegionName string `db:"l3.public_name_ua"`
+	}
+
+	Category struct {
+		ID        uuid.UUID `db:"id"`
+		NameEN    string    `db:"name_en"`
+		NameRU    string    `db:"name_ru"`
+		NameUA    string    `db:"name_ua"`
+		CreatedAt time.Time `db:"created_at"`
+	}
+
+	Help struct {
+		ID         uuid.UUID `db:"id"`
+		CreatorID  uuid.UUID `db:"creator_id"`
+		CategoryID uuid.UUID `db:"category_id"`
+		LocalityID int       `db:"locality_id"`
+		CreatedAt  time.Time `db:"created_at"`
+		DeletedAt  time.Time `db:"deleted_at"`
+	}
+)
+
+const (
+	upsertUserSQL = `
+insert into user
+(id, tg_id, chat_id, name, created_at, updated_at)
+values ($1, $2, $3, $4, $5, %6)`
+
+	// todo: search by different languages
+	selectLocalitiesSQL = `
+SELECT l1.id, l1.type, l1.public_name_ua, l3.public_name_ua from locality as l1
+    left join locality as l2 on (l1.parent_id = l2.id)
+    left join locality as l3 on (l2.parent_id = l3.id)
+where levenshtein(l1.name_ua, $1) <= 1
+  AND l1.type != 'DISTRICT' AND l1.type != 'STATE' AND l1.type != 'COUNTRY';`
+)
+
+func (p *Postgres) UpsertUser(user *User) (*User, error) {
+	var (
+		now = time.Now().UTC()
+		uid = uuid.New()
+	)
+
+	_, err := p.driver.ExecContext(p.ctx, upsertUserSQL,
+		uid, user.TgID, user.ChatID, user.Name, now, now)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:        uid,
+		TgID:      user.TgID,
+		ChatID:    user.ChatID,
+		Name:      user.Name,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}, nil
+}
+
+func (p *Postgres) SelectLocalities(s string) ([]*LocalityRegion, error) {
+	rows, err := p.driver.Queryx(selectLocalitiesSQL, s)
+	if err != nil {
+		return nil, err
+	}
+
+	var localities = make([]*LocalityRegion, 0)
+	for rows.Next() {
+		l := new(LocalityRegion)
+		err := rows.Scan(l)
+		if err != nil {
+			return nil, err
+		}
+
+		localities = append(localities, l)
+	}
+
+	return localities, nil
+}
+
+func (p *Postgres) SelectRequestsByUser(u uuid.UUID) ([]*Request, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p *Postgres) InsertRequest(request *Request) (*Request, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p *Postgres) ResolveRequest(u uuid.UUID) error {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p *Postgres) SelectHelpsByUser(u uuid.UUID) ([]*Help, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p *Postgres) InsertHelp(help *Help) (*Help, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (p *Postgres) DeleteHelp(u uuid.UUID) error {
+	// TODO implement me
+	panic("implement me")
 }
